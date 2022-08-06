45789575eca12c674c541fa815d00690d81efad0
Vita Iskovskikh <vitaiskovskikh@gmail.com>
2022-08-06 22:44:59 +0300

    Create a project

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int balanceInitial = 100;//Начальная сумма у клиента
+        int bonusAmount = 1000;//Бонусная сумма для пополнения
+        int refill = 1100;//Сумма пополнения клиентом
+        int bonus = 0;//Бонусы начисленные клиенту
+
+        if (refill >= bonusAmount) {
+            bonus = refill/100;//Бонусы начисленные клиенту
+        }
+
+        int balanceFinal = balanceInitial + refill + bonus;//Итоговая сумма боланса после пополнения
+
+        System.out.println("Итоговая сумма:");
+        System.out.println(balanceFinal + "рублей");
+    }
+
+}
